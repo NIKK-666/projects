fbd10da46618245dca4028f64a9da6b46aaa72ed
Nikk <146761471+NIKK-666@users.noreply.github.com>
2024-06-10 21:34:07 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -32,3 +32,51 @@ anime
     easing: "easeOutExpo",
     delay: 1000,
   });
+
+  
+  // const yesButton = document.getElementById('Yes')
+  // const noButton = document.getElementById('No')
+  const buttons = document.querySelectorAll('.btn');
+ 
+
+  buttons.forEach((button) => {
+    button.addEventListener('mouseenter', () => {
+      // const yesIndex = Array.from(cta.children).indexOf('Yes');
+      // const noIndex = Array.from(cta.children).indexOf('No');
+      
+      // if(yesIndex < noIndex){
+      //   cta.insertBefore('No','Yes');
+      // }else{
+      //   cta.insertBefore('Yes','No');
+      // }
+
+       if (button.textContent === 'No') {
+      //  if(yesIndex < noIndex){
+        button.textContent = 'Yes';
+      } else {
+      
+        button.textContent = 'No';
+      }
+    });
+  });
+
+//   const buttons = document.querySelectorAll('.btn');
+
+// buttons.forEach((button, index) => {
+//   button.addEventListener('mouseover', () => {
+//     const buttonTexts = ['Yes', 'No','Yes', 'No'];
+//     button.textContent = buttonTexts[(index + 1) % 3];
+//   });
+// });
+
+// const buttons = document.querySelectorAll('.btn');
+
+// buttons.forEach((button, index) => {
+//   button.addEventListener('mouseover', () => {
+//     if (index === 0) {
+//       button.textContent = 'No';
+//     } else {
+//       button.textContent = 'Yes';
+//     }
+//   });
+// });
